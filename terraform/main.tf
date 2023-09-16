c9b1d5ba1c222282f81f210bdf0b7ac5345ff764
Ariel Filotti <filotti@gmail.com>
2023-09-16 01:00:55 -0400

    Add comments

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,9 @@
+# Enable Cloud Resource Manager API for the project
 resource "google_project_service" "cloud_resource_manager" {
   service = "cloudresourcemanager.googleapis.com"
 }
 
+# Enable Compute Engine API for the project. Also, use a local exec provisioner to remove default firewall rules.
 resource "google_project_service" "compute" {
   service = "compute.googleapis.com"
   provisioner "local-exec" {
@@ -9,21 +11,28 @@ resource "google_project_service" "compute" {
       gcloud compute firewall-rules list --format 'value(name)' --project=${var.project_id} | xargs gcloud compute firewall-rules delete -q
     EOT
   }
+  # Ensure Cloud Resource Manager is enabled before proceeding
   depends_on = [google_project_service.cloud_resource_manager]
 }
 
+# Create a custom VPC without default subnets
 resource "google_compute_network" "main" {
   name                            = "vpc"
   auto_create_subnetworks         = false
   delete_default_routes_on_create = true
-  depends_on                      = [google_project_service.compute]
+
+  # Ensure the Compute API is enabled before creating the VPC
+  depends_on = [google_project_service.compute]
 }
 
+# Create a custom subnet within the above VPC
 resource "google_compute_subnetwork" "main" {
   region        = "us-east1"
   ip_cidr_range = "10.0.0.0/16"
   name          = "subnet"
   network       = google_compute_network.main.self_link
+
+  # Configure advanced VPC Flow Logs with a 5-second aggregation interval
   log_config {
     aggregation_interval = "INTERVAL_5_SEC"
     flow_sampling        = 1
@@ -31,12 +40,14 @@ resource "google_compute_subnetwork" "main" {
   }
 }
 
+# Setup a router within the VPC for Cloud NAT
 resource "google_compute_router" "router" {
   name    = "cloud-router"
   region  = "us-east1"
   network = google_compute_network.main.name
 }
 
+# Define the Cloud NAT configuration for outbound connectivity
 resource "google_compute_router_nat" "nat" {
   name                               = "cloud-nat"
   region                             = "us-east1"
@@ -45,6 +56,7 @@ resource "google_compute_router_nat" "nat" {
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
 
+# Firewall rule to allow internal traffic within the VPC
 resource "google_compute_firewall" "allow_internal" {
   name          = "allow-internal"
   network       = google_compute_network.main.name
@@ -65,6 +77,7 @@ resource "google_compute_firewall" "allow_internal" {
   }
 }
 
+# Firewall rule to allow SSH access via Identity-Aware Proxy (IAP)
 resource "google_compute_firewall" "allow_iap" {
   name          = "allow-iap"
   network       = google_compute_network.main.name
@@ -78,6 +91,7 @@ resource "google_compute_firewall" "allow_iap" {
   }
 }
 
+# Create a shared SSD with multi-writer enabled
 resource "google_compute_disk" "shared" {
   provider     = google-beta
   name         = "shared"
@@ -85,9 +99,12 @@ resource "google_compute_disk" "shared" {
   zone         = "us-east1-d"
   size         = 10
   multi_writer = true
-  depends_on   = [google_project_service.compute]
+
+  # Ensure the Compute API is enabled before creating the disk  
+  depends_on = [google_project_service.compute]
 }
 
+# Define the first Compute Instance "nas-1" with the shared disk attached
 resource "google_compute_instance" "nas-1" {
   machine_type = "n2-standard-2"
   name         = "nas-1"
@@ -107,6 +124,7 @@ resource "google_compute_instance" "nas-1" {
   }
 }
 
+# Define the second Compute Instance "nas-2" with the shared disk attached
 resource "google_compute_instance" "nas-2" {
   machine_type = "n2-standard-2"
   name         = "nas-2"
@@ -124,6 +142,6 @@ resource "google_compute_instance" "nas-2" {
   network_interface {
     subnetwork = google_compute_subnetwork.main.self_link
   }
+  # Ensure "nas-1" instance is created before creating "nas-2"
   depends_on = [google_compute_instance.nas-1]
 }
-
